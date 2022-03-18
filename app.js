22480c229e3c4d70b08a595ff22329d1b9408a6a
angeldarco <angel92-eme@hotmail.com>
2022-03-18 08:18:39 +0000

    Version 2.0 finished

--- app.js
+++ app.js
@@ -1,48 +1,76 @@
 const container = document.querySelector('.container');
+let R = '00'
+let G = '00'
+let B = '00'
+let A = 'FF'
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('result').addEventListener('click', () => { change() });
+  comprobaton()
 })
 
-function change() {
-  let R = document.getElementById('R').value;
-  let G = document.getElementById('G').value;
-  let B = document.getElementById('B').value;
-  let A = document.getElementById('A').value;
-
-  let RGB = [R, G, B, A];
-  let out = ''
-  let out02 = '';
-
-  //  Example In: 0, 36, 255
-  //  Example out: #0024ff
-  
-  let conunt = 0
-  let arr = [];
-  let hexd = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-
-  for (let z = 0; z < RGB.length; z++) {
-    if (RGB[3] == '') RGB[3] = 255
-    if (RGB[z] == undefined) RGB[z] = 0
-    
-    if (RGB[z] == 0) {
-      out = '00';
-      conunt = 0
-    }
-    for (; RGB[z] > 0; RGB[z] -= hexd.length) {
-      if (RGB[z] > hexd.length) {
-        arr.push(conunt);
-        out = hexd[arr.length];
-      } else {
-        out += hexd[RGB[z]].toString()
-        arr = []
-
+function change(name, value) {
+  valor = parseInt(value)
+  let hexd = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let data = ''
+  let a = 0
+if (value == undefined){
+  data = '00'
+}else{
+  if (value <= hexd.length - 1) {
+    data = '0'+hexd[value]
+  }else {
+    while (valor >= 16) {
+      if (valor >= 16) {
+        a++
+        valor -= 16
       }
-      conunt++
     }
-    out02 += out;
-    conunt = 0
-  } //main for
+    data = hexd[a].toString() + hexd[valor].toString()
+  }
+}
 
-  document.getElementById('hex').innerHTML = '#'+out02;
-  container.style.background = '#' + out02;
+  switch (name) {
+    case 'R':
+      R = data
+      break;
+    case 'G':
+      G = data
+      break;
+    case 'B':
+      B = data
+      break;
+    case 'A':
+      A = data
+      break;
+      default:
+      break;
+  }
+  if (R === '00' && G === '00' && B === '00' && A == 'FF' ) 
+    A = '00'
+  else if (A == '00')
+    A = 'FF'
+  else if (A != '00' && A != 'FF')
+    A = A
+
+  document.getElementById('hex').innerHTML = '#'+R+G+B+A;
+  container.style.background = '#' + R+G+B+A;
+
+}
+
+function comprobaton() {
+  const inputs = document.querySelectorAll('input')
+  inputs.forEach(input => {
+    input.addEventListener('keyup', (e) => {
+      if (e.target.value > 255 || e.target.value[0] == 0 ) {
+        e.target.style.border = '1px solid red';
+        e.target.style.background = 'red';
+      } else if (e.target.value == '') {
+        change(e.target.id, undefined)
+      } else {
+        e.target.style.border = '1px';
+        e.target.style.background = 'green';
+        change(e.target.id, e.target.value)
+      }
+
+    })
+  });
 }
